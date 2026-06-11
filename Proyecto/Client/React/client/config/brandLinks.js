b9ab1ca72e957d4e2a3da48a4e6b00556d317975
Eduardo Gomez <eduman.000@gmail.com>
2026-06-11 20:24:56 -0300

    mailupdate

--- Proyecto/Client/React/client/config/brandLinks.js
+++ Proyecto/Client/React/client/config/brandLinks.js
@@ -4,7 +4,7 @@ export const BRAND_LINKS = {
   instagramBrand: "https://www.instagram.com/harta_bymale/",
   instagramOwner: "https://www.instagram.com/maleearballo/",
   whatsappPhone: "5493471566912",
-  email: "mailto:contacto@harta.com",
+  email: "mailto:malena.arballo1@gmail.com",
 };
 
 export const BRAND_WHATSAPP_URL = `https://wa.me/${BRAND_LINKS.whatsappPhone}?text=${encodeURIComponent(
